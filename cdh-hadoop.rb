3ca242b52dbf8c1813111cc72521745edd8a36d4
Brennon York <brennon.york@gmail.com>
2014-05-17 22:36:27 -0400

    Moved CDH Hadoop homebrew formulas into this repository

---
+++ cdh-hadoop.rb
@@ -0,0 +1,74 @@
+require 'formula'
+
+class CdhHadoop < Formula
+  homepage 'http://archive.cloudera.com/cdh4/cdh/4/hadoop-2.0.0-cdh4.5.0/'
+  url 'http://archive.cloudera.com/cdh4/cdh/4/hadoop-2.0.0-cdh4.5.0.tar.gz'
+  sha1 '17e73e63d666daf9cbe803e4f92d9494dd11217d'
+  version '4.5.0'
+
+  depends_on 'protobuf'
+  
+  def shim_script target
+    <<-EOS.undent
+    #!/bin/bash
+    exec "#{libexec}/bin/#{target}" "$@"
+    EOS
+  end
+
+  def install
+    libexec.install %w[bin etc lib libexec sbin share]
+    bin.mkpath
+    Dir["#{libexec}/bin/*"].each do |b|
+      n = Pathname.new(b).basename
+      (bin+n).write shim_script(n)
+    end
+
+    inreplace "#{libexec}/etc/hadoop/hadoop-env.sh",
+      "export HADOOP_IDENT_STRING=$USER",
+      "
+export HADOOP_IDENT_STRING=$USER
+export HADOOP_OPTS=\"$HADOOP_OPTS -Djava.security.krb5.realm=OX.AC.UK -Djava.security.krb5.kdc=kdc0.ox.ac.uk:kdc1.ox.ac.uk\"
+"
+
+    inreplace "#{libexec}/etc/hadoop/yarn-env.sh",
+      "YARN_OPTS=\"$YARN_OPTS -Dyarn.policy.file=$YARN_POLICYFILE\"",
+      "
+YARN_OPTS=\"$YARN_OPTS -Dyarn.policy.file=$YARN_POLICYFILE\"
+YARN_OPTS=\"$YARN_OPTS -Djava.security.krb5.realm=OX.AC.UK -Djava.security.krb5.kdc=kdc0.ox.ac.uk:kdc1.ox.ac.uk\"
+"
+
+    inreplace "#{libexec}/etc/hadoop/core-site.xml",
+      "</configuration>",
+      "
+  <property>
+    <name>hadoop.tmp.dir</name>
+    <value>/Users/${user.name}/hadoop-store</value>
+  </property>
+  <property>
+    <name>fs.default.name</name>
+    <value>hdfs://localhost:8020</value>
+  </property>
+</configuration>
+"
+
+    inreplace "#{libexec}/etc/hadoop/hdfs-site.xml",
+      "</configuration>",
+      "
+  <property>
+    <name>dfs.replication</name>
+    <value>1</value>
+  </property>
+</configuration>
+"
+
+    inreplace "#{libexec}/etc/hadoop/log4j.properties",
+      "# Custom Logging levels",
+      "# Custom Logging levels
+log4j.logger.org.apache.hadoop.util.NativeCodeLoader=ERROR
+"
+  end
+  
+  def test
+    system "hadoop", "version"
+  end
+end
---
+++ cdh-mr1.rb
@@ -0,0 +1,58 @@
+require 'formula'
+
+class CdhMr1 < Formula
+  homepage 'http://www.cloudera.com'
+  url 'http://archive.cloudera.com/cdh4/cdh/4/mr1-2.0.0-mr1-cdh4.2.1.tar.gz'
+  sha1 '0996957fe771e044fd01e378988ced4d5a7e4f00'
+  version '4.2.1'
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin conf lib webapps contrib]
+    libexec.install Dir['*.jar']
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
+    # But don't make rcc visible, it conflicts with Qt
+    (bin/'rcc').unlink
+
+    inreplace "#{libexec}/conf/hadoop-env.sh",
+      "export HADOOP_IDENT_STRING=$USER",
+      "
+export HADOOP_IDENT_STRING=$USER
+export HADOOP_OPTS=\"$HADOOP_OPTS -Djava.security.krb5.realm=OX.AC.UK -Djava.security.krb5.kdc=kdc0.ox.ac.uk:kdc1.ox.ac.uk\"
+"
+
+    inreplace "#{libexec}/conf/core-site.xml",
+      "</configuration>",
+      "
+  <property>
+    <name>hadoop.tmp.dir</name>
+    <value>/Users/${user.name}/hadoop-store</value>
+  </property>
+  <property>
+    <name>fs.default.name</name>
+    <value>hdfs://localhost:8020</value>
+  </property>
+</configuration>
+"
+
+    inreplace "#{libexec}/conf/mapred-site.xml",
+      "</configuration>",
+      "
+  <property>
+    <name>mapred.job.tracker</name>
+    <value>hdfs://localhost:9001</value>
+  </property>
+</configuration>
+"
+
+    inreplace "#{libexec}/conf/log4j.properties",
+      "# Custom Logging levels",
+      "# Custom Logging levels
+log4j.logger.org.apache.hadoop.util.NativeCodeLoader=ERROR
+"
+  end
+  
+  def test
+    system "hadoop", "version"
+  end
+end
